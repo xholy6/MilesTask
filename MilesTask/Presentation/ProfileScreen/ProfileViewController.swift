95ed10d9a919f494b7b5eca9003d465fbdae41e6
xholy6 <blast6xgit@gmail.com>
2023-09-08 10:18:51 +0300

    added qrcode button

--- MilesTask/Presentation/ProfileScreen/ProfileViewController.swift
+++ MilesTask/Presentation/ProfileScreen/ProfileViewController.swift
@@ -23,6 +23,7 @@ final class ProfileViewController: UIViewController {
         static let cornerRadius: CGFloat = 12
         static let cellHeight: CGFloat = 56
         static let cellCount = 6
+        static let qrcodeButtonSize: CGFloat = 72
     }
 
     var viewModel = ProfileViewModel()
@@ -52,6 +53,17 @@ final class ProfileViewController: UIViewController {
         button.tintColor = .black
         button.setTitleColor(.black, for: .normal)
         button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        return button
+    }()
+
+    private lazy var qrcodeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "qrcode"), for: .normal)
+        button.backgroundColor = .mRed
+        let padding: CGFloat = 16
+        button.contentEdgeInsets = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+
         return button
     }()
 
@@ -91,6 +103,7 @@ final class ProfileViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(profileTableView)
         view.addSubview(quitButton)
+        view.addSubview(qrcodeButton)
     }
 
     private func activateConstraints() {
@@ -105,7 +118,15 @@ final class ProfileViewController: UIViewController {
             quitButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: edge),
             quitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -edge),
             quitButton.heightAnchor.constraint(equalToConstant: 48),
+
+            qrcodeButton.topAnchor.constraint(equalTo: quitButton.bottomAnchor, constant: 32),
+            qrcodeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            qrcodeButton.heightAnchor.constraint(equalToConstant: Constants.qrcodeButtonSize),
+            qrcodeButton.widthAnchor.constraint(equalToConstant: Constants.qrcodeButtonSize),
         ])
+
+        qrcodeButton.layer.cornerRadius = Constants.qrcodeButtonSize/2
+        qrcodeButton.layer.masksToBounds = true
     }
 }
 
